0666df99fff92407d93fee61821884f9d94feff1
Adarsh Srivastava <adrshsrvstv@gmail.com>
2025-03-01 01:27:04 +0800

    Added set -e and messages for each step

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
 
+set -e
+
 BRANCH=$(hostname -s)
+
+echo "Cloning repo to $HOME/temp-dotfiles ..."
 
 git clone -b $BRANCH --single-branch --separate-git-dir=$HOME/.dotfiles git@github.com:adrshsrvstv/.dotfiles.git $HOME/temp-dotfiles
 
+echo "Cloned repo. Putting the files back to $HOME ..."
+
 rsync --recursive --verbose --exclude '.git' $HOME/temp-dotfiles/ $HOME/
 
+echo "Deleting $HOME/temp-dotfiles ..."
+
 rm -rf $HOME/temp-dotfiles
+
+echo "Setting git status.showUntrackedFiles to no ..."
 
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
--- setup.sh
+++ setup.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
 BRANCH=$(hostname -s)
+
+echo "Setting git push.autoSetupRemote to true ..."
 
 git config --global --type bool push.autoSetupRemote true
 
+echo "Initializing a bare git repo at $HOME/.dotfiles ..."
+
 git init --bare $HOME/.dotfiles
+
+echo "Setting git status.showUntrackedFiles to no ..."
 
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
+echo "Checking out a new branch..."
+
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout --orphan $BRANCH
+
+echo "Adding the remote origin..."
 
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME remote add origin git@github.com:adrshsrvstv/.dotfiles.git
 
-echo "Done. Export your alias and then add your dotfiles as shown in the README."
+echo "Done! Execute the following to add the 'dotfiles' alias to your shell: alias dotfiles='git --git-dir=\$HOME/.dotfiles/ --work-tree=\$HOME'"
